ee52bd5692bc47958fea901c0735cbc77b3bcc8c
jenny-min <56142904+jenny-min@users.noreply.github.com>
2021-09-08 10:02:24 +0700

    update files

---
+++ app.js
@@ -0,0 +1,14 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+
+function addGoal() {
+    const enterValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enterValue;
+    listEl.appendChild(listItemEl);
+    inputEl.value = '';
+}
+
+buttonEl.addEventListener('click', addGoal);
